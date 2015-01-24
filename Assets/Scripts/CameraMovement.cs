c08551815012360ac62a267d8e801e251e2b4c4c
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-01-24 21:43:44 +0100

    CameraMovement script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+  public float speed = 1.0f;
+
+  // Use this for initialization
+  void Start () {
+  }
+
+  // Update is called once per frame
+  void Update () {
+    float horizontalAxis = Input.GetAxis("Horizontal");
+    float verticalAxis = Input.GetAxis("Vertical");
+    float delta = Time.deltaTime;
+    float horizontalDistance = speed*delta*horizontalAxis;
+    float verticalDistance = speed*delta*verticalAxis;
+    float x = transform.position.x;
+    float y = transform.position.y;
+    float z = transform.position.z;
+    x += horizontalDistance;
+    z += verticalDistance;
+    transform.position = new Vector3(x, y, z);
+  }
+}
