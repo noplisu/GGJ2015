c644718295b44a8aaa53cb402190d431816f4629
glisowski <lisux1991@tlen.pl>
2015-01-25 02:12:33 +0100

    Pause Menu added

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuController : MonoBehaviour {
+	GameObject pauseMenu;
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Time.timeScale = 0.00000001f;
+			pauseMenu.SetActive(true);
+		}
+	}
+
+	public void resume()
+	{
+		Time.timeScale = 1f;
+		pauseMenu.SetActive(false);
+	}
+
+	public void mainMenu()
+	{
+		Application.LoadLevel(0);
+	}
+	
+	public void exit()
+	{
+		Application.Quit();
+	}
+}
