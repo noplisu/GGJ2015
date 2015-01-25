bdce1159186178a631056b98b48df8b75ce6aea9
glisowski <lisux1991@tlen.pl>
2015-01-25 01:31:21 +0100

    Simple main menu added

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+	public void start()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void exit()
+	{
+		Application.Quit();
+	}
+}
