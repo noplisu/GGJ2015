82e063f5ef20ce1ee8b676da73e167a405b318b8
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-01-25 04:17:07 +0100

    SelectCharacter script

---
+++ Assets/Scripts/SelectCharacter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectCharacter : MonoBehaviour {
+  public GameObject current = null;
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (Input.GetMouseButton(0)) {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit)) {
+        Transform transform = hit.transform;
+        if (transform.tag == "Player") {
+          current = transform.gameObject;
+        }
+      }
+    }
+  }
+}
